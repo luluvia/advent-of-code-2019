d60fe5631c5090702521bd9044c35970717db168
Lulu Yume <lu@luyu.me>
2019-12-01 12:52:50 -0700

    feat: day 1 part 2, also amend part 1 with separate function

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,31 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+// Part 1 code.
+
+fn add_fuel_one(mass: u32) -> u32 {
+    let fuel = mass / 3 - 2;
+    return fuel;
+}
+
+// Part 2 code.
+
+fn add_fuel_two(mass: u32) -> u32 {
+    let mut fuel = 0;
+
+    match (mass / 3).checked_sub(2) {
+        None => fuel = 0,
+        Some(sub) => fuel = sub
+    }
+
+    if fuel == 0 {
+        return fuel;
+    }
+    else {
+        return fuel + add_fuel_two(fuel);
+    }
+}
+
 fn main() {
     const FILE_NAME: &str = "day1_input.txt";
 
@@ -12,7 +37,7 @@ fn main() {
 
     for (_index, line) in reader.lines().enumerate() {
         let mass: u32 = line.unwrap().parse().unwrap();
-        let required_fuel: u32 = mass / 3 - 2;
+        let required_fuel: u32 = add_fuel_two(mass);
         fuel_total += required_fuel;
     }
 
